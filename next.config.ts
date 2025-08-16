3d944d5321fbd3591ba3fe5d4840e6e264be44d3
growbranz <growbranz5@gmail.com>
2025-08-16 22:29:52 +0530

    updated the gallery and hero file

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    unoptimized: true // disables Next.js image optimization
-  },
+  /* config options here */
 };
 
 export default nextConfig;
